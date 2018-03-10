462f49ab610ed8fe87fad10f2d0cbdb09ce053e2
Lennart Wenke <lennart.wenke@arcor.de>
2018-03-10 22:13:46 +0100

    committed by crontab

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 function login() {
 	var txt = document.getElementById("pass").value;
 	if (txt.length > 0) {
-		if (txt == "wetter") {
+		if (txt == "wetter18") {
 			document.getElementById("success").style.display = "block";
 			document.getElementById("hidden").style.display = "block";
 			document.getElementById("login").style.display = "none";
