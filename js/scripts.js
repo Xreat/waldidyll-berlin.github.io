a1257e808d1f9b5d0544f5bd172e037ab14a2527
Lennart Wenke <lennart.wenke@arcor.de>
2019-01-19 00:52:02 +0100

    committed by crontab

--- js/scripts.js
+++ js/scripts.js
@@ -45,49 +45,66 @@ const refreshInterval = 300000;
 let refresh = window.setInterval(injectData, refreshInterval);
 
 function injectData(event) {
-	document.querySelectorAll('*[id]').forEach(e => {
-		let id = e.getAttribute("id");
-		if (e.tagName === "CANVAS") { setGaugeValue(id, db[e.getAttribute("data-tag")]) }
-		else if (db[id]) { e.innerHTML = db[id]; }
-	});
-	
-	let m = document.querySelectorAll("select")[0];
-	db["months"].forEach(e => {
-		let row = document.createElement('option');
-		row.innerHTML = e;
-		row.value = e;
-		m.appendChild(row);
-	});
-	let y = document.querySelectorAll("select")[1];
-	db["years"].forEach(e => {
-		let row = document.createElement('option');
-		row.innerHTML = e;
-		row.value = e;
-		y.appendChild(row);
-	});
-	
-	document.querySelector("input[type=checkbox]").onchange = function(evt) {
-		if (evt.target.checked && !refresh) {
-			refresh = window.setInterval(injectData, refreshInterval);
-		} 
-		else if (!evt.target.checked && refresh) {
-			window.clearInterval(refresh);
-			refresh = 0;
-		}
-	}
-	
-	const loc = db["station.location"] || "";
-	document.querySelector("header p a").src = "https://www.google.com/maps/?q=" + loc;
-	// document.title = loc + " " + document.title;
-	document.querySelector("[property='og:site_name']").content = loc;
-	document.querySelector("[property='og:description']").content = `Weekly Weather Summary from ${loc} - weather web site powered by weewx`;
-	
-	if (document.querySelector(".login-page")) {
-		document.querySelector(".login-page button").onclick = login;
-		document.querySelector(".login-page input").onkeyup = function(evt) {
-			if (evt.keyCode === 13) {
-				document.querySelector(".login-page button").click();
+	loadJSON(function(response) {
+		// Parse JSON string into object
+		let db = JSON.parse(response);
+		document.querySelectorAll('*[id]').forEach(e => {
+			let id = e.getAttribute("id");
+			if (e.tagName === "CANVAS") { setGaugeValue(id, db[e.getAttribute("data-tag")]) }
+			else if (db[id]) { e.innerHTML = db[id]; }
+		});
+		
+		let m = document.querySelectorAll("select")[0];
+		db["months"].forEach(e => {
+			let row = document.createElement('option');
+			row.innerHTML = e;
+			row.value = e;
+			m.appendChild(row);
+		});
+		let y = document.querySelectorAll("select")[1];
+		db["years"].forEach(e => {
+			let row = document.createElement('option');
+			row.innerHTML = e;
+			row.value = e;
+			y.appendChild(row);
+		});
+		
+		document.querySelector("input[type=checkbox]").onchange = function(evt) {
+			if (evt.target.checked && !refresh) {
+				refresh = window.setInterval(injectData, refreshInterval);
+			} 
+			else if (!evt.target.checked && refresh) {
+				window.clearInterval(refresh);
+				refresh = 0;
 			}
 		}
-	}
+		
+		const loc = db["station.location"] || "";
+		document.querySelector("header p a").src = "https://www.google.com/maps/?q=" + loc;
+		// document.title = loc + " " + document.title;
+		document.querySelector("[property='og:site_name']").content = loc;
+		document.querySelector("[property='og:description']").content = `Weekly Weather Summary from ${loc} - weather web site powered by weewx`;
+		
+		if (document.querySelector(".login-page")) {
+			document.querySelector(".login-page button").onclick = login;
+			document.querySelector(".login-page input").onkeyup = function(evt) {
+				if (evt.keyCode === 13) {
+					document.querySelector(".login-page button").click();
+				}
+			}
+		}
+	});
 }
+
+ function loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'js/values.json', true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
